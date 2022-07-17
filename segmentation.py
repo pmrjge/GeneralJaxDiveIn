171e75f33f227f96456f5a6c144f6c74f30572cb
Pedro Jorge <pmrjge@gmail.com>
2022-07-17 23:07:50 +0100

    Started implementing training logic

--- segmentation.py
+++ segmentation.py
@@ -59,7 +59,7 @@ class TrainLoader:
 
 import multiprocessing
 
-batch_size = 8
+batch_size = 4
 num_cpus = min([max([1,int(multiprocessing.cpu_count() * 0.8)]), int(batch_size * 0.8)])
 
 tl = TrainLoader()
@@ -163,9 +163,140 @@ class ConvInverse(hk.Module):
         w_init = hki.VarianceScaling(1.0)
         b_init = hki.Constant(1e-6)
 
-        x1, x2, x3, x4, x5, x_reduced = x
+        x1, x2, x3, x4, x5, x6, x7, x8, x_reduced = x
+
+        val = hk.Conv2DTranspose(4096, 1, stride=1, padding="SAME", w_init=w_init, b_init=b_init)(x_reduced)
+        zcat = jnp.concatenate([val, x8], axis=3)
+        val = hk.Conv2D(output_channels=4096, kernel_shape=1, stride=1, padding="SAME", w_init=w_init, b_init=b_init)(zcat)
+        val = self.bn()(val, is_training)
+        val = jnn.gelu(val)
+
+        val = hk.Conv2DTranspose(2048, 3, stride=2, padding="SAME", w_init=w_init, b_init=b_init)(val)
+        zcat = jnp.concatenate([val, x7], axis=3)
+        val = hk.Conv2D(output_channels=2048, kernel_shape=3, stride=1, padding="SAME", w_init=w_init, b_init=b_init)(zcat)
+        val = self.bn()(val, is_training)
+        val = jnn.gelu(val)
+
+        val = hk.Conv2DTranspose(2048, 3, stride=2, padding="SAME", w_init=w_init, b_init=b_init)(val)
+        zcat = jnp.concatenate([val, x6], axis=3)
+        val = hk.Conv2D(output_channels=2048, kernel_shape=3, stride=1, padding="SAME", w_init=w_init, b_init=b_init)(zcat)
+        val = self.bn()(val, is_training)
+        val = jnn.gelu(val)
+
+        val = hk.Conv2DTranspose(1024, 3, stride=2, padding="SAME", w_init=w_init, b_init=b_init)(val)
+        zcat = jnp.concatenate([val, x5], axis=3)
+        val = hk.Conv2D(output_channels=1024, kernel_shape=3, stride=1, padding="SAME", w_init=w_init, b_init=b_init)(zcat)
+        val = self.bn()(val, is_training)
+        val = jnn.gelu(val)
+
+        val = hk.Conv2DTranspose(512, 3, stride=2, padding="SAME", w_init=w_init, b_init=b_init)(val)
+        zcat = jnp.concatenate([val, x4], axis=3)
+        val = hk.Conv2D(output_channels=512, kernel_shape=3, stride=1, padding="SAME", w_init=w_init, b_init=b_init)(zcat)
+        val = self.bn()(val, is_training)
+        val = jnn.gelu(val)
+
+        val = hk.Conv2DTranspose(256, 3, stride=2, padding="SAME", w_init=w_init, b_init=b_init)(val)
+        zcat = jnp.concatenate([val, x3], axis=3)
+        val = hk.Conv2D(output_channels=256, kernel_shape=3, stride=1, padding="SAME", w_init=w_init, b_init=b_init)(zcat)
+        val = self.bn()(val, is_training)
+        val = jnn.gelu(val)
+
+        val = hk.Conv2DTranspose(256, 3, stride=2, padding="SAME", w_init=w_init, b_init=b_init)(val)
+        zcat = jnp.concatenate([val, x2], axis=3)
+        val = hk.Conv2D(output_channels=256, kernel_shape=3, stride=1, padding="SAME", w_init=w_init, b_init=b_init)(zcat)
+        val = self.bn()(val, is_training)
+        val = jnn.gelu(val)
+
+        val = hk.Conv2DTranspose(128, 3, stride=2, padding="SAME", w_init=w_init, b_init=b_init)(val)
+        zcat = jnp.concatenate([val, x1], axis=3)
+        val = hk.Conv2D(output_channels=128, kernel_shape=3, stride=1, padding="SAME", w_init=w_init, b_init=b_init)(zcat)
+        val = self.bn()(val, is_training)
+        val = jnn.gelu(val)
+
+        val = hk.Conv2DTranspose(128, 3, stride=2, padding="SAME", w_init=w_init, b_init=b_init)(val)
+        val = hk.Conv2D(output_channels=1, kernel_shape=1, stride=1, padding="SAME", w_init=w_init, b_init=b_init)(val)
+        val = self.bn()(val, is_training)
+
+        return val
 
 
+class SimpleUNet(hk.Module):
+    def __init__(self, dropout=0.4, name: Optional[str] = None):
+        super().__init__(name)
+        self.dropout = dropout
+
+    def __call__(self, x, is_training):
+        dropout = self.dropout if is_training else 0.0
+
+        w_init = hki.VarianceScaling(1.0)
+        b_init = hki.Constant(1e-6)
+
+        x1, x2, x3, x4, x5, x6, x7, x8 = ConvSimplifier()(x, is_training)
+
+        x_reduced = hk.Conv2D(4096, 1, 1, padding="SAME", w_init=w_init, b_init=b_init)
+        x_reduced = hk.dropout(hk.next_rng_key(), dropout, x_reduced)
+        x_reduced = jnn.gelu(x_reduced)
+
+        mask = ConvInverse()((x1, x2, x3, x4, x5, x6, x7, x8, x_reduced), is_training)
+
+        return mask
 
 
+def build_forward_fn(dropout=0.5):
+    def forward_fn(x: jnp.ndarray, is_training: bool = True) -> jnp.ndarray:
+        return SimpleUNet(dropout)(x, is_training=is_training)
 
+    return forward_fn
+
+@ft.partial(jax.jit, static_argnums=(0, 6))
+def lm_loss_fn(forward_fn, params, state, rng, x, y, is_training: bool = True):
+    y_pred, state = forward_fn(params, state, rng, x, is_training)
+
+    l2_loss = 0.1 * sum(jnp.sum(jnp.square(p)) for p in jax.tree_util.tree_leaves(params))
+    return optax.sigmoid_binary_cross_entropy(y_pred, y) + dice_loss(y_pred, y, smooth=1e-6) + 1e-4 * l2_loss, state
+
+class GradientUpdater:
+    def __init__(self, net_init, loss_fn, optimizer: optax.GradientTransformation):
+        self._net_init = net_init
+        self._loss_fn = loss_fn
+        self._opt = optimizer
+
+    def init(self, master_rng, x):
+        out_rng, init_rng = jax.random.split(master_rng)
+        params, state = self._net_init(init_rng, x)
+        opt_state = self._opt.init(params)
+        return jnp.array(0), out_rng, params, state, opt_state
+
+    def update(self, num_steps, rng, params, state, opt_state, x: jnp.ndarray, y: jnp.ndarray):
+        rng, new_rng = jax.random.split(rng)
+
+        (loss, state), grads = jax.value_and_grad(self._loss_fn, has_aux=True)(params, state, rng, x, y)
+
+        grads = jax.lax.pmean(grads, axis_name='j')
+
+        updates, opt_state = self._opt.update(grads, opt_state, params)
+
+        params = optax.apply_updates(params, updates)
+
+        metrics = {
+            'step': num_steps,
+            'loss': loss,
+        }
+
+        return num_steps + 1, new_rng, params, state, opt_state, metrics
+
+
+def replicate_tree(t, num_devices):
+    return jax.tree_map(lambda x: jnp.array([x] * num_devices), t)
+
+
+logging.getLogger().setLevel(logging.INFO)
+grad_clip_value = 1.0
+learning_rate = 0.001
+batch_size = 4
+dropout = 0.5
+max_steps = 8000
+num_devices = jax.local_device_count()
+rng = jr.PRNGKey(111)
+
+train_loader = TrainLoader()
