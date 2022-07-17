5424cbb6fd85d70651f498ea8b923dd7c9c1b5cb
Pedro Jorge <pmrjge@gmail.com>
2022-07-17 05:00:20 +0100

    Simplified the mask computation and the retrieving of image and mask

--- segmentation.py
+++ segmentation.py
@@ -22,41 +22,24 @@ from PIL import Image
 
 train = pd.read_csv('/kaggle/input/hubmap-organ-segmentation/train.csv')
 
-def rle_to_mask(rle, width, target_size=None):
-    if target_size == None:
-        target_size = width
+def get_mask(image_id, size):
+    row = train.loc[train['id'] == image_id].squeeze()
+    h, w = row[['img_height', 'img_width']]
+    mask = np.zeros(shape=[h * w], dtype=np.int32)
+    s = row['rle'].split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
 
-    rle = np.array(list(map(int, rle.split())))
-    label = np.zeros((width*width))
-    
-    for start, end in zip(rle[::2], rle[1::2]):
-        label[start:start+end] = 1
+    starts -= 1
+    ends = starts + lengths
+    for lo, hi in zip(starts, ends):
+        mask[lo : hi] = 1
+    mask = mask.reshape([h, w]).T
+
+    mask = cv2.resize(mask, [size, size], interpolation=cv2.INTER_CUBIC).astype(np.int32)
+
+    mask = np.expand_dims(mask, axis=2)
         
-    label = Image.fromarray(label.reshape(width, width))
-
-    label = label.resize((target_size, target_size))
-    label = np.array(label).astype(float)
-
-    label = np.round((label - label.min())/(label.max() - label.min()))
-    
-    return label.T
-
-def mask_to_rle(mask, orig_dim=160):
-
-    size = int(len(mask.flatten())**.5)
-    n = Image.fromarray(mask.reshape((size, size)))
-    n = n.resize((orig_dim, orig_dim))
-    n = np.array(n).astype(np.float32)
-
-    pixels = n.T.flatten()
-
-    pixels = (pixels-min(pixels) > ((max(pixels)-min(pixels))/2)).astype(int)
-    
-    pixels = np.concatenate([[0], pixels, [0]])
-    runs = np.where(pixels[1:] != pixels[:-1])[0]
-    runs[1::2] -= runs[::2]
-    
-    return ' '.join(str(x) for x in runs)
+    return mask
 
 class TrainLoader:
     def __init__(self):
@@ -69,14 +52,57 @@ class TrainLoader:
     def get_sample(self, idx):
         path = self.paths[idx]
         image = Image.open(str(path))
-        image = image.resize((self.img_size, self.img_size))
+        image = cv2.resize(image, [self.img_size, self.img_size], interpolation=cv2.INTER_CUBIC).astype(np.int32)
         image = np.array(image).astype(float)
         image = (image - image.min()) / (image.max() - image.min())
         image = jnp.array(image)
 
-        organ = jnp.array([train.organ.iloc8idx])
-
-        label = rle_to_mask(train.rle[idx], train.img_width[idx], self.img_size)
+        label = get_mask(idx, self.img_size)
         label = jnp.array(label)
 
-        return image, organ, label
+        return image, label
+
+
+import multiprocessing
+
+num_cpus = max([1,int(multiprocessing.cpu_count() * 0.7)])
+
+
+def get_generator_parallel(rng_key, batch_size, num_devices):
+
+
+    def get_data(tl, perm):
+        pool = multiprocessing.Pool(processes=num_cpus)
+
+        perm = np.array(perm).tolist()
+        m = lambda p: tl.get_sample(p)
+
+        outputs_async = pool.map_async(m, perm)
+        pool.close()
+        pool.join()
+        outputs = outputs_async.get()
+        
+        x, o, y = zip(*outputs)
+
+        x = jnp.vstack(x, dtype=jnp.float32)
+
+        o = jnp.vstack(o, dtype=jnp.int32)
+
+        y = jnp.vstack(y)
+
+        return x, o, y
+
+    def batch_generator():
+        n = x.shape[0]
+        key = rng_key
+        kk = batch_size // num_devices
+        tl = TrainLoader()
+        while True:
+            key, k1 = jax.random.split(key)
+            perm = jax.random.choice(k1, n, shape=(batch_size,))
+
+            x, o, y = get_data(tl, perm)
+
+            yield x.reshape(num_devices, kk, *x.shape[1:]), y.reshape(num_devices, kk, *y.shape[1:])
+
+    return batch_generator()
