359a98f76e431faf81af74d107e654a4418f7266
Pedro Jorge <pmrjge@gmail.com>
2022-07-28 00:29:47 +0100

    Attempted the best way to implement in flax as a beginner coder, now jumping to do the same in dm-haiku

--- 01_flax_sentiment_analysis/00_transformer_from_scratch.py
+++ 01_flax_sentiment_analysis/00_transformer_from_scratch.py
@@ -15,9 +15,6 @@ import numpy as np
 import einops as eps
 
 
-
-
-
 class SelfAttention(fnn.Module):
     embed_size: int
     n_heads: int
@@ -25,7 +22,7 @@ class SelfAttention(fnn.Module):
     b_init: Callable = fi.zeros
 
     @fnn.compact
-    def __call__(self, v, k, q, mask=None):
+    def __call__(self, v, k, q):
         # Compute head dimension and guarantee embed_size is divisible by n_heads to split the input
         head_dim = self.embed_size // self.n_heads
         assert (head_dim * self.n_heads == self.embed_size)
@@ -40,18 +37,15 @@ class SelfAttention(fnn.Module):
         value_len, key_len, query_len = v.shape[1], k.shape[1], q.shape[1]
         # Split embeddings into self.heads pieces
 
-        v = v.reshape(n, value_len, self.n_heads, self.head_dim)
-        k = k.reshape(n, key_len, self.n_heads, self.head_dim)
-        q = q.reshape(n, query_len, self.n_heads, self.head_dim)
+        v = v.reshape(n, value_len, self.n_heads, head_dim)
+        k = k.reshape(n, key_len, self.n_heads, head_dim)
+        q = q.reshape(n, query_len, self.n_heads, head_dim)
 
         v = values(v)
         k = keys(k)
         q = query(q)
 
         energy = jnp.einsum('...qhd,...khd->...hqk', q, k)
-
-        if mask is not None:
-            energy = jnp.where(mask == 0, -jnp.inf, energy)
 
         attention = fnn.softmax(energy / (self.embed_size ** 0.5), axis=3)
 
@@ -70,7 +64,7 @@ class TransformBlock(fnn.Module):
     b_init: Callable = fi.zeros
 
     @fnn.compact
-    def __call__(self, value, key, query, mask,  *, train):
+    def __call__(self, value, key, query, *, train):
         dr = self.dropout_rate if train else 0.0
         attention = SelfAttention(embed_size=self.embed_size, n_heads=self.n_heads)
         norm1 = fnn.LayerNorm(bias_init=fi.zeros, scale_init=fi.ones)
@@ -82,11 +76,11 @@ class TransformBlock(fnn.Module):
             fnn.Dense(self.embed_size, kernel_init=self.w_init, bias_init=self.b_init),
         ])
 
-        a0 = attention(value, key, query, mask)
+        a0 = attention(value, key, query)
         sc = norm1(a0 + query)
         sc = fnn.Dropout(dr)(sc, deterministic=not train)
-        f = ff(sc)
-        sc1 = norm2(f + sc)
+        fout = ff(sc)
+        sc1 = norm2(fout + sc)
         return fnn.Dropout(dr)(sc1)
 
 
@@ -95,9 +89,9 @@ class PositionalEmbedding(fnn.Module):
     embed_size: int
 
     def setup(self):
-        pe = jnp.zeros(self.max_seq_len, self.embed_size)
+        pe = jnp.zeros((self.max_seq_len, self.embed_size))
         for pos in range(self.max_seq_len):
-            for i in range(0, self.embed_dim, 2):
+            for i in range(0, self.embed_size, 2):
                 pe = pe.at[pos, i].set(jnp.sin(pos / (10000 ** ((2 * i) / self.embed_size))))
                 pe = pe.at[pos, i+1].set(jnp.cos(pos / (10000 ** ((2 * (i+1)) / self.embed_size))))
 
@@ -122,13 +116,13 @@ class Encoder(fnn.Module):
     b_init: Callable = fi.zeros
 
     @fnn.compact
-    def __call__(self, x, mask,  *, train):
+    def __call__(self, x, *, train):
         dr = self.dropout_rate if train else 0.0
 
         # Define layers
         word_e = fnn.Embed(self.vocab_size, self.embed_size)
         pos_e = PositionalEmbedding(max_seq_len=self.seq_len, embed_size=self.embed_size)
-        blocks = [TransformBlock(embed_size=self.embed_size, n_heads=self.n_heads, forward_expansion=self.forward_expansion, dropout_rate=self.dropout_rate) for _ in range(self.num_layers)]
+        blocks =[TransformBlock(embed_size=self.embed_size, n_heads=self.n_heads, forward_expansion=self.forward_expansion, dropout_rate=self.dropout_rate) for _ in range(self.num_layers)]
         fc = fnn.Sequential([fnn.Dense(512, kernel_init=self.w_init, bias_init=self.b_init), fnn.Dense(256, kernel_init=self.w_init, bias_init=self.b_init), fnn.Dense(2, kernel_init=self.w_init, bias_init=self.b_init)])
 
         # Compute
@@ -137,7 +131,7 @@ class Encoder(fnn.Module):
         e = fnn.Dropout(dr)(e, deterministic=not train)
 
         for block in blocks:
-            e = block(e, e, e, mask, train=train)
+            e = block(e, e, e, train=train)
 
         return fc(e)
         # - fnn.logsumexp(logits)
@@ -153,8 +147,6 @@ class TransformerEncoder(fnn.Module):
     seq_len: int = 60
     pad_idx: int = 0
 
-    def make_mask(self, x):
-        return jnp.expand_dims(x != self.pad_idx, axis=(1, 2))
 
     @fnn.compact
     def __call__(self, x, *, train):
@@ -162,9 +154,7 @@ class TransformerEncoder(fnn.Module):
                           n_heads=self.n_heads, forward_expansion=self.forward_expansion, dropout_rate=self.dropout_rate,
                           seq_len=self.seq_len)
 
-        mask = self.make_mask(x)
-
-        return encoder(x, mask, train=train)
+        return encoder(x, train=train)
 
 
 # Load data
