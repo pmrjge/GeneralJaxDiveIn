cc6cf6f04eb615d17f80982e10a8f12edce2cd38
Pedro Jorge <pmrjge@gmail.com>
2022-07-28 01:06:58 +0100

    Started working on haiku for the transformer in sentiment analysis

---
+++ 01_flax_sentiment_analysis/01_transformer_from_scratch_haiku.py
@@ -0,0 +1,48 @@
+import pickle
+import functools as ft
+
+import jax
+import jax.random as jr
+import jax.numpy as jnp
+import jax.nn as jnn
+
+import optax
+
+import einops as nps
+
+import numpy as np
+
+import haiku as hk
+import haiku.initializers as hki
+
+
+class SelfAttention(hk.Module):
+    def __init__(self, dim):
+        self.dim = dim
+        self.factor = dim ** -0.5
+        super(SelfAttention, self).__init__()
+
+    @staticmethod
+    def masked_fill(mask, a, fill):
+        return jax.lax.select(mask, jax.lax.broadcast(fill, a.shape), a)
+
+    def __call__(self, x, mask=None):
+        assert len(x.shape) == 3, '3D Tensor must be provided'
+
+        w_init = hki.VarianceScaling()
+        qkv = hk.Linear(output_size=self.dim * 3, with_bias=False, w_init=w_init, b_init=hki.Constant(0))(x)
+
+        q, k, v = tuple(nps.rearrange(qkv, 'b t (d k) -> k b t d ', k=3))
+
+        scaled_dot_prod = self.factor * jnp.einsum('b i d,b j d -> b i j', q, k)
+
+        if mask is not None:
+            assert mask.shape == scaled_dot_prod.shape[1:], "Mask needs to have same shape as scaled dot product"
+            # Other version might be:
+            # scaled_dot_prod = jnp.where(mask, -jnp.inf, scaled_dot_prod)
+            scaled_dot_prod = jax.lax.select(mask, jax.lax.broadcast(-jnp.inf, scaled_dot_prod.shape), scaled_dot_prod)
+
+        attention = jnn.softmax(scaled_dot_prod, axis=-1)
+
+        return jnp.einsum('b i j, b j d -> b i d', attention, v)
+
