dd2bc968d842efa68048feb4c100d78aa1730d2c
Pedro Jorge <pmrjge@gmail.com>
2022-07-31 18:43:22 +0100

    Attempting at accelerating the training phase with RectifiedAdam

--- 01_vision_transformer/00_vision_transformer.py
+++ 01_vision_transformer/00_vision_transformer.py
@@ -236,26 +236,26 @@ def ce_loss_fn(forward_fn, params, state, rng, a, b, num_classes: int = 10):
     soft_f10 = 2 * tn / (2 * tn + fn + fp + 1e-16)
     cost1 = 1 - soft_f11
     cost0 = 1 - soft_f10
-    f1_cost = jnp.mean(0.5 * (cost1 + cost0))
+    f1_loss = jnp.mean(0.5 * (cost1 + cost0))
 
     # cosine similarity
 
 
     # soft f1 score loss + focal loss and weight decay and l1 loss
-    return jnp.log(0.5 * f1_cost + 0.5 * f_loss + 1.0 + 1e-16) + 1e-14 * (l2_loss + l1_loss), state
+    return 0.5 * f_loss + 0.5 * jnp.log(f1_loss + 1.0 + 1e-16) + 1e-14 * (l2_loss + l1_loss), state
 
 
 loss_fn = ft.partial(ce_loss_fn, l_apply)
 
-learning_rate = 1e-4
+learning_rate = 1e-2
 grad_clip_value = 1.0
 # scheduler = optax.exponential_decay(init_value=learning_rate, transition_steps=6000, decay_rate=0.99)
 
 optimizer = optax.chain(
     optax.adaptive_grad_clip(grad_clip_value),
     # optax.sgd(learning_rate=learning_rate, momentum=0.99, nesterov=True),
-    # optax.scale_by_radam(b1=0.9, eps=1e-8),
-    optax.scale_by_adam(b1=0.9, eps=1e-6),
+    optax.scale_by_radam(b1=0.9, eps=1e-2),
+    #optax.scale_by_adam(b1=0.9, eps=1e-6),
     # optax.scale_by_yogi(),
     # optax.scale_by_schedule(scheduler),
     optax.scale(-learning_rate)
